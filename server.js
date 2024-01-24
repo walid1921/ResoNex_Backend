4fe7df156c1b98bd379e01a3d86ed7444f2218e0
user.walid1921 <user.kouiderayadwalid@gmail.com>
2024-01-24 20:53:06 +0100

    cors

--- server.js
+++ server.js
@@ -33,27 +33,28 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const MONGO_URL = process.env.MONGO_URL;
 const FRONTEND = process.env.FRONTEND_URL;
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested, Content-Type, Accept Authorization"
+//   )
+//   if (req.method === "OPTIONS") {
+//     res.header(
+//       "Access-Control-Allow-Methods",
+//       "POST, PUT, PATCH, GET, DELETE"
+//     )
+//     return res.status(200).json({})
+//   }
+//   next()
+// })
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const corsOptions = {
   origin: FRONTEND,
   optionsSuccessStatus: 200,
+  credentials: true,
 };
 
 //! middleware
